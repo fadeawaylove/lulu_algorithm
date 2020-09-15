3d21f77e1b070d86cf051e40fa7bc1d6aabb0e45
邓润庭 <dengrt@akulaku.com>
2020-09-15 12:11:03 +0800

    子集Ⅱ

--- 算法/回溯法/codes/17_子集II2.py
+++ 算法/回溯法/codes/17_子集II2.py
@@ -22,27 +22,32 @@
 """
 
 from typing import List
+"""
+使用迭代法
+原数组[1,2,2]
+[] -> []
+[1] -> [] [1]
+[1,2] -> [] [1]  - [2] [1,2]
+[1,2,2] -> [] [1] [2] [1,2] {[2] [1,2]} [2,2] [1,2,2] 
+"""
 
 
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         nums = sorted(nums)
-        res = []
-
-        def trace_back(start, path, total):
-            if len(path) == total:
-                res.append(path)
-                return
-            for i in range(start, n):
-                # 只需判断是否和上一个数字相同
-                if nums[i] == nums[i-1] and i > start:
+        res = [[]]
+        start = 0
+        for i in range(n):
+            temp = []
+            for j in range(len(res)):
+                if i > 0 and nums[i] == nums[i - 1] and j <= start:
                     continue
-                trace_back(i + 1, path + [nums[i]], total)
-
-        for x in range(n + 1):
-            trace_back(0, [], x)
+                temp.append(res[j] + [nums[i]])
+            start = len(res) - 1
+            res.extend(temp)
         return res
 
 
 print(Solution().subsetsWithDup([4, 4, 4, 1, 4]))
+# print(Solution().subsetsWithDup([1, 2, 2]))
